352c24c07c6c35c09c54d17003f9bf670fb2cbe3
Bo Gao <bogaogit@gmail.com>
2024-10-11 14:57:33 +1000

    c

--- src/components/Lobby.jsx
+++ src/components/Lobby.jsx
@@ -1,18 +1,11 @@
-import {useSocket} from '@/context/SocketProvider';
-import React, {useEffect, useState} from 'react';
+import React from 'react';
 import RoomPage from "@/components/RoomPage";
 
 const LobbyScreen = () => {
-    const [showPage, setShowPage] = useState(false);
-    const socket = useSocket();
 
-    useEffect(() => {
-        socket.emit('room:join', {email: Math.random(), room: 1});
-        socket.on("room:join", () => setShowPage(true));
-    }, []);
 
     return (
-        showPage && <RoomPage></RoomPage>
+        <RoomPage></RoomPage>
     )
 }
 
